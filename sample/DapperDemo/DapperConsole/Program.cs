a0be1a460e951dcf1de097640fde724e5e5b71fd
oppoic <oppoic@163.com>
2019-06-12 22:38:55 +0800

    commit

---
+++ sample/DapperDemo/DapperConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DapperConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
